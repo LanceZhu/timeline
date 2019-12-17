94ee64d628403b593ab6297a7499d92c1341b894
LanceZhu <lancezhu2937@gmail.com>
2019-12-17 13:28:58 +0800

    style: update preface

--- src/page/timeline/default.vue
+++ src/page/timeline/default.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="container-timeline-default">
-    <div v-html="content"></div>
+    <div v-html="content" class="content"></div>
     <div class="feedback">
       <el-divider></el-divider>
-      遇到问题请联系：e_chronicle2019@163.com
+      遇到问题请联系：ee_chronicle2019@163.com
       <!-- <a href="mailto:ee_chronicle2019@163.com">ee_chronicle2019@163.com</a> -->
     </div>
   </div>
 </template>
 
 <script>
+import { preface } from '../../../config'
+
 export default {
   data () {
     return {
-      content: ''
+      content: preface
     }
   }
 }
 </script>
 
 <style scoped>
-.container{
+.content{
   width: 90%;
   margin: 0 auto;
   text-align: left;
+  white-space: pre-wrap;
 }
 .feedback{
   color: gray;
