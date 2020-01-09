c3f38b1c99ac35e010aea564b1b18437a01be09d
LanceZhu <lancezhu2937@gmail.com>
2020-01-09 17:58:01 +0800

    refactor: rm useless file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,28 +1,15 @@
 import Home from '../page/home/index'
 
-import HelloWorld from '../components/HelloWorld'
-
-import Wikis from '../page/wikis/index'
-import Wiki from '@/page/wiki/index'
-import WikiView from '@/page/wiki/view'
-import WikiAdd from '../page/wiki/add'
-import WikiEdit from '@/page/wiki/edit'
-import WikiHistory from '@/page/wiki/history'
-
-import Index from '../page/index/index'
-
 import Login from '../page/login/index'
-
-import Timepoint from '../page/timepoint/index'
-import TimepointView from '@/page/timepoint/view'
-
-import Search from '@/page/search/index'
-
-import NotFound from '@/page/404/index'
 
 import Timeline from '@/page/timeline/index'
 import TimelineDefault from '@/page/timeline/default'
 import TimelineView from '@/page/timeline/view'
+
+const Search = () => import('@/page/search/index')
+
+const NotFound = () => import('@/page/404/index')
+
 const TimepointAdd = () => import('@/page/timepoint/add')
 const TimepointEdit = () => import('@/page/timepoint/edit')
 const TimepointHistory = () => import('@/page/timepoint/history')
@@ -64,51 +51,9 @@ export default [{
         component: TimepointHistory
       }]
     }, {
-      path: 'index',
-      name: 'index',
-      component: Index
-    }, {
-      path: 'helloworld',
-      name: 'helloworld',
-      component: HelloWorld
-    }, {
-      path: 'wikis',
-      name: 'wikis',
-      component: Wikis
-    }, {
-      path: 'wiki',
-      name: 'wiki',
-      component: Wiki
-    }, {
-      path: 'wiki/view/:id',
-      name: 'wiki',
-      component: WikiView
-    }, {
-      path: 'wiki/add',
-      name: 'wiki',
-      component: WikiAdd,
-      meta: { requiresAuth: true }
-    }, {
-      path: 'wiki/edit/:id',
-      name: 'wiki',
-      component: WikiEdit,
-      meta: { requiresAuth: true }
-    }, {
-      path: 'wiki/history/:id',
-      name: 'wiki',
-      component: WikiHistory
-    }, {
       path: 'login',
       name: 'login',
       component: Login
-    }, {
-      path: 'timepoint',
-      name: 'timepoint',
-      component: Timepoint
-    }, {
-      path: 'timepoint/view/:id',
-      name: 'timepoint',
-      component: TimepointView
     }, {
       path: 'timepoint/add/:id',
       name: 'timepoint',
