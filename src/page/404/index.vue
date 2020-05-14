c7ed94dbc42fa5a85882fa48aec5d39664a6c1de
LanceZhu <lancezhu2937@gmail.com>
2020-05-14 10:28:32 +0800

    feat: update 404 page

--- src/page/404/index.vue
+++ src/page/404/index.vue
@@ -1,3 +1,18 @@
 <template>
-    <div>404 Not Found!</div>
+    <div class="container">
+        <div class="title">
+            404 未找到该页面
+        </div>
+        <br/>
+        <a href="/">返回首页</a>
+    </div>
 </template>
+
+<style scoped>
+.container {
+    margin-top: 40px;
+}
+.title {
+    font-size: 20px;
+}
+</style>
