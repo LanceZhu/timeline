56a0e915f85faf0c0ff2bcce4950ca4e514e32b6
LanceZhu <lancezhu2937@gmail.com>
2020-06-10 10:19:09 +0800

    feat: add reason before edit timepoint

--- src/page/timepoint/edit.vue
+++ src/page/timepoint/edit.vue
@@ -1,16 +1,51 @@
 <template>
   <div class="container-edit">
-    <FuzzyTimePicker ref="FuzzyTimePicker" :defaultValue="this.dateValue" :defaultDate="this.date" :defaultType="this.dateType"></FuzzyTimePicker>
-    <Editor ref="Editor" :defaultTitle="this.title" :defaultContent="this.content"></Editor>
-    <NationalityAndInventor v-if="this.$view.showNationalityAndInventor" ref="NationalityAndInventor" :nationality="ruleForm.nationality" :inventor="ruleForm.inventor"></NationalityAndInventor>
-    <Tags ref="Tags" :defaultTagsChoosed="this.tagsChoosed" editable></Tags>
-    <Citation ref="Citation" :defaultCitations="this.citations" editable></Citation>
+    <FuzzyTimePicker ref="FuzzyTimePicker" :defaultValue="timepoint.dateValue" :defaultDate="timepoint.date" :defaultType="timepoint.dateType"></FuzzyTimePicker>
+    <Editor ref="Editor" :defaultTitle="timepoint.title" :defaultContent="timepoint.content"></Editor>
+    <NationalityAndInventor v-if="this.$view.showNationalityAndInventor" ref="NationalityAndInventor" :nationality="timepoint.nationality" :inventor="timepoint.inventor"></NationalityAndInventor>
+    <Tags ref="Tags" :defaultTagsChoosed="timepoint.tagsChoosed" editable></Tags>
+    <Citation ref="Citation" :defaultCitations="timepoint.citations" editable></Citation>
     <div class="submit">
-      <el-button type="primary" @click="submit()">提交</el-button>
+      <el-button type="primary" @click="beforeSubmitForm.visible = true">提交</el-button>
+      <el-dialog
+        title="修改原因"
+        :visible.sync="beforeSubmitForm.visible"
+        width="70%">
+        <el-form
+          :model="beforeSubmitForm"
+          :rules="beforeSubmitFormRules"
+          label-position="top"
+          ref="beforeSubmitForm"
+          style="text-align: left">
+          <el-form-item label="变更类型（可多选）" prop="checkedOptions">
+            <div>
+              <el-checkbox-group v-model="beforeSubmitForm.checkedOptions" size="mini">
+                <el-checkbox v-for="option in beforeSubmitForm.options" :label="option" :key="option" border style="margin-left: 0; margin-top: 5px"></el-checkbox>
+              </el-checkbox-group>
+            </div>
+          </el-form-item>
+          <el-form-item label="补充信息" prop="reason">
+            <el-input
+              type="textarea"
+              v-model="beforeSubmitForm.reason"
+              :autosize="{ minRows: 3, maxRows: 6 }"
+              maxlength="500"
+              show-word-limit
+              placeholder="请简要描述变更内容，以便于管理员审核。">
+            </el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer">
+          <el-button @click="beforeSubmitForm.visible = false">取 消</el-button>
+          <el-button type="primary" @click="beforeSubmit('beforeSubmitForm')">确 定</el-button>
+        </span>
+      </el-dialog>
     </div></div>
 </template>
 
 <script>
+import updateTimeline from '@/utils/updateTimeline'
+
 const NationalityAndInventor = () => import('@/components/NationalityAndInventor')
 const FuzzyTimePicker = () => import('@/components/FuzzyTimePicker')
 const Citation = () => import('@/components/Citation')
@@ -20,15 +55,27 @@ const Editor = () => import('@/components/Editor')
 export default {
   data () {
     return {
-      // 以组件形式嵌入的表单字段集合
-      ruleForm: {},
-      title: '',
-      content: '',
-      dateValue: [100], // 时间点显示值
-      dateType: 100,
-      date: '',
-      tagsChoosed: [],
-      citations: []
+      // 仅用于初始化
+      timepoint: {
+        dateValue: [100], // 时间点显示值
+        dateType: 100,
+        date: '',
+        title: '',
+        content: '',
+        tagsChoosed: [],
+        citations: []
+      },
+      beforeSubmitForm: {
+        visible: false,
+        reason: '',
+        checkedOptions: [],
+        options: ['丰富内容', '更正错误', '时间信息', '标题', '正文', '标签', '参考文献', '其他']
+      },
+      beforeSubmitFormRules: {
+        checkedOptions: [
+          { type: 'array', required: true, message: '请至少选择一个变更类型', trigger: 'change' }
+        ]
+      }
     }
   },
   components: {
@@ -38,7 +85,22 @@ export default {
     Tags,
     Editor
   },
+  async created () {
+    await this.init()
+  },
   methods: {
+    async beforeSubmit (formName) {
+      const resBoolean = await this.$refs[formName].validate()
+      if (!resBoolean) {
+        return false
+      }
+      this.beforeSubmitForm.visible = false
+      await this.submit()
+    },
+    async afterSubmit () {
+      this.$router.push(`/timeline/${res.data.new_post_id}`)
+      await updateTimeline()
+    },
     async submit () {
       // 时间点选择字段
       const { year, month, day, show } = this.$refs.FuzzyTimePicker.getData()
@@ -75,6 +137,11 @@ export default {
       // 标题和内容字段
       const { title, content } = this.$refs.Editor.getData()
 
+      let reason = this.beforeSubmitForm.checkedOptions.join(';')
+      if (this.beforeSubmitForm.reason !== '') {
+        reason += `;${this.beforeSubmitForm.reason}`
+      }
+
       // post 请求参数
       const params = {
         title,
@@ -84,7 +151,8 @@ export default {
         day,
         show,
         tag,
-        supplement: citations
+        supplement: citations,
+        reason
       }
 
       Object.assign(params, nationalityAndCreator)
@@ -96,25 +164,8 @@ export default {
             message: '编辑成功',
             type: 'success'
           })
-          setTimeout(() => {
-            this.$router.push(`/timeline/${res.data.new_post_id}`)
-            this.$axios.get('/api/timepoint/list').then(res => {
-              if (res.data.code === 100) {
-                let timeline = res.data.data
-                timeline = timeline.map(time => {
-                  if (typeof time.show === 'object') {
-                    const { show } = time.show
-                    if (show) {
-                      time.show = time.show.show
-                    } else {
-                      time.show = time.show.date
-                    }
-                  }
-                  return time
-                })
-                this.$store.commit('updateTimeline', timeline)
-              }
-            })
+          setTimeout(async () => {
+            await this.afterSubmit()
           })
           break
         }
@@ -124,25 +175,25 @@ export default {
       }
     },
     dateInitialize (dateInput) {
-      this.dateValue = dateInput.type
-      const dateType = this.dateValue.reduce((acc, cur) => {
+      this.timepoint.dateValue = dateInput.type
+      const dateType = this.timepoint.dateValue.reduce((acc, cur) => {
         acc += cur
         return acc
       })
-      this.date = dateInput.date
-      this.dateType = dateType
-    }
-  },
-  async created () {
+      this.timepoint.date = dateInput.date
+      this.timepoint.dateType = dateType
+    },
     // 已有数据初始化
-    try {
+    async init () {
       const res = await this.$axios.get(`/api/timepoint/show/${this.$route.params.id}`)
       const { title, content, tag = [], supplement, show, nationality, inventor } = res.data.data.post
-      this.title = title
-      this.content = content
-      this.citations = supplement
+      Object.assign(this.timepoint, {
+        title,
+        content,
+        citations: supplement
+      })
       try {
-        this.tagsChoosed = tag
+        this.timepoint.tagsChoosed = tag
       } catch (err) {
         console.error(err)
       }
@@ -154,12 +205,10 @@ export default {
           date: show
         })
       }
-      Object.assign(this.ruleForm, {
+      Object.assign(this.timepoint, {
         nationality,
         inventor
       })
-    } catch (err) {
-      console.error(err)
     }
   }
 }
