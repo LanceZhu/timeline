4ac731f228da899712a093136055d1a16797cae9
LanceZhu <lancezhu2937@gmail.com>
2020-06-28 18:29:41 +0800

    feat: add deploy.sh

---
+++ scripts/deploy.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+set -e
+
+starttime=`date +'%Y-%m-%d %H:%M:%S'`
+
+echo 'eechina start'
+npm run build:eechina > /dev/null
+scp -r dist/* welishi:/home/ubuntu/wiki/wiki-docker-master/frontend/frontend-eechina > /dev/null
+echo -e "eechina end"
+
+echo 'edudata start'
+npm run build:edudata > /dev/null
+scp -r dist/* welishi:/home/ubuntu/wiki/wiki-docker-master/frontend/frontend-edudata > /dev/null
+echo 'edudata end'
+
+echo 'aidata start'
+npm run build:aidata > /dev/null
+scp -r dist/* welishi:/home/ubuntu/wiki/wiki-docker-master/frontend/frontend-aidata > /dev/null
+echo 'aidata end'
+
+git checkout infor
+echo 'infor start'
+npm run build:infor > /dev/null
+scp -r dist/* welishi:/home/ubuntu/wiki/wiki-docker-master/frontend/frontend-infor > /dev/null
+echo 'infor end'
+
+git checkout aidata
+
+endtime=`date +'%Y-%m-%d %H:%M:%S'`
+start_seconds=$(date --date="$starttime" +%s)
+end_seconds=$(date --date="$endtime" +%s)
+echo "本次运行时间： "$((end_seconds-start_seconds))"s"
