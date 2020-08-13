86f83765ef81e1530283e777fd7f64cc35e231ba
LanceZhu <lancezhu2937@gmail.com>
2020-08-13 22:21:47 +0800

    feat: add cis config

---
+++ config/cis.js
@@ -0,0 +1,12 @@
+import defaultConfig from './default'
+
+const preface = '时间轴 Wiki - 指挥信息系统技术与应用'
+
+const websiteName = '指挥信息系统技术与应用'
+
+const config = Object.assign({}, defaultConfig, {
+  preface,
+  websiteName
+})
+
+export default config
