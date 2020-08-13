28171b892e319a025e138380904fea41399b36fa
LanceZhu <lancezhu2937@gmail.com>
2020-08-13 22:07:10 +0800

    feat: add info in scripts

--- scripts/flushCDN.sh
+++ scripts/flushCDN.sh
@@ -2,6 +2,6 @@
 # flush tencent cloud cdn
 # tccli https://cloud.tencent.com/document/product/440/39027
 # flush path api https://cloud.tencent.com/document/product/228/37871
-set -e
+set -ex
 
 tccli cdn PurgePathCache --Paths '["http://aidata.welishi.cn/", "http://edudata.welishi.cn/", "http://eechina.welishi.cn", "http://infor.welishi.cn"]' --FlushType flush
