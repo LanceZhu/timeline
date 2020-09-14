20f1f05f1514cd110535846025a5b442530604f7
LanceZhu <lancezhu2937@gmail.com>
2020-09-14 16:25:06 +0800

    feat: lint && add disucss part in timepoint page

--- src/page/discuss/list.vue
+++ src/page/discuss/list.vue
@@ -3,7 +3,8 @@
     <div class="menu-title">
       历史分析
     </div>
-    <div v-for="(thread, index) in threads" :key="index" class="thread-wrapper" v-loading="loading">
+    <div v-if="threads.length === 0" style="padding: 5px;">暂无分析文章</div>
+    <div v-else v-for="(thread, index) in threads" :key="index" class="thread-wrapper" v-loading="loading">
         <router-link :to="`/discuss/${thread._id}`" tag="li">
           <div class="thread">
             <div>
@@ -29,14 +30,24 @@ export default {
     }
   },
   async created () {
-    const threads = await this.getThreadsList()
+    const timepointId = this.getTimepointId()
+
+    const threads = await this.getThreadsList(timepointId)
     this.threads = threads
 
     this.loading = false
   },
   methods: {
-    async getThreadsList () {
-      const res = await this.$axios.get('api/discuss/getThreadsList')
+    getTimepointId () {
+      return this.$route.query.timepointId
+    },
+    async getThreadsList (timepointId) {
+      let API = 'api/discuss/getThreadsList'
+      if (timepointId) {
+        API = `${API}/${timepointId}`
+      }
+
+      const res = await this.$axios.get(API)
       const { threads } = res.data.data
 
       threads.forEach(thread => {
@@ -61,7 +72,7 @@ export default {
   padding: 0 20px;
   padding-top: 50px;
   background-color: #fafafa;
-  border-radius: 20rpx;
+  border-radius: 20px;
 }
 
 .menu-title {
@@ -71,7 +82,7 @@ export default {
   text-align: left;
 }
 
-.menu-title:before {
+.menu-title::before {
   content: '';
   border-left: 4px solid #409eff;
   position: absolute;
